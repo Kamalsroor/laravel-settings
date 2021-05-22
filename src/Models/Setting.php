b553db232bed6721909cefe2bef3b1014b204f08
laratrist <kamal.s.sroor@gmail.com>
2021-05-22 10:22:58 +0200

    add media size

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -3,9 +3,15 @@
 namespace Laraeast\LaravelSettings\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Setting extends Model
+class Setting extends Model implements HasMedia
 {
+
+    use InteractsWithMedia;
+    use HasUploader;
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -14,4 +20,42 @@ class Setting extends Model
     protected $fillable = [
         'key', 'locale', 'value',
     ];
+
+
+      /**
+     * Define the media collections.
+     *
+     * @return void
+     */
+    public function registerMediaCollections(): void
+    {
+        // $this->addMediaCollection('logo')->singleFile();
+
+        $this
+        ->addMediaCollection('logo')
+        // ->useFallbackUrl('https://www.gravatar.com/avatar/'.md5($this->email).'?d=mm')
+        ->singleFile()
+        ->registerMediaConversions(function () {
+            $this->addMediaConversion('thumb')
+                ->width(70)
+                ->format('png');
+
+            $this->addMediaConversion('small')
+                ->width(120)
+                ->format('png');
+
+            $this->addMediaConversion('medium')
+                ->width(160)
+                ->format('png');
+
+            $this->addMediaConversion('large')
+                ->width(320)
+                ->format('png');
+        });
+
+
+        $this->addMediaCollection('favicon')->singleFile();
+    }
+
+
 }
